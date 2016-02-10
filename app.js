ae864b710c5dab9cfa450b91016ccc6435288320
jerecockrell <jere.cockrell@gmail.com>
2016-02-10 15:52:35 -0700

    create todo table

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 console.log('hello world')
 
 function sayItWorked () {
-	var $element = $(event.target);
-	var $form = $element.closest('form');
+	event.preventDefault();
+	var $event = $(event.target);
+	var $form = $event.closest('form');
 	var $inputs = $form.find('input');
-	
-	console.log('It worked!', $inputs);
 
-	var value1 = $inputs.first().val();
-	var value2 = $inputs.eq(1).val();
+	var taskName = $inputs.first().val();
+	var dueDate = $inputs.last().val();
+
+if(taskName !== "" || dueDate !== ''){
+	
+	var $list = $('#todoTable');
+	$list.append('<tr> \
+  				<td>' + taskName + '</td> \
+  				<td>' + dueDate +'</td> \
+  				<td><input type="checkbox"></td> \
+  				 </tr>');
+				
+}
+
+
 
 	console.log('Submitted with ', value1, ' and ', value2);
 	return false;
